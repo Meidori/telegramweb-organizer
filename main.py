9e2d1dbd0183c316686d8ea8a67afb423fadae8d
Meidori <mdr@xd.com>
2025-06-11 03:06:40 +0500

    test index

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__, template_folder='.')
 def web():  
     return render_template('index.html')  
   
-if __name__ == "__main__":  
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port='80')  
