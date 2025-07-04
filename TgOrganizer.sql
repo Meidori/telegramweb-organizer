33c299d7971b248580d02ee94ec6c9f52446ed6e
Meidori <mdr@xd.com>
2025-07-04 06:43:37 +0500

    sql dump

---
+++ TgOrganizer.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: amvera-meidori-run-usersorganaizer
+-- Время создания: Июл 04 2025 г., 00:29
+-- Версия сервера: 8.1.0
+-- Версия PHP: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `TgOrganizer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Category`
+--
+
+CREATE TABLE `Category` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `color_hex` varchar(7) NOT NULL DEFAULT '#FFFFFF'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `DayEntry`
+--
+
+CREATE TABLE `DayEntry` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `entry_date` datetime NOT NULL,
+  `category_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `User`
+--
+
+CREATE TABLE `User` (
+  `id` int NOT NULL,
+  `telegram_id` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Триггеры `User`
+--
+DELIMITER $$
+CREATE TRIGGER `create_categories` AFTER INSERT ON `User` FOR EACH ROW BEGIN
+	INSERT INTO Category (user_id, name, color_hex)
+    VALUES
+    	(NEW.id, 'Работа', '#808080'),
+        (NEW.id, 'Учеба', '#0e3b0e'),
+        (NEW.id, 'Спорт', '#00008b'),
+        (NEW.id, 'Хобби', '#9400d3'),
+        (NEW.id, 'Отдых', '#ff2b2b');
+END
+$$
+DELIMITER ;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Category`
+--
+ALTER TABLE `Category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `DayEntry`
+--
+ALTER TABLE `DayEntry`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Индексы таблицы `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `telegram_id` (`telegram_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Category`
+--
+ALTER TABLE `Category`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `DayEntry`
+--
+ALTER TABLE `DayEntry`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `Category`
+--
+ALTER TABLE `Category`
+  ADD CONSTRAINT `Category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `DayEntry`
+--
+ALTER TABLE `DayEntry`
+  ADD CONSTRAINT `DayEntry_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `DayEntry_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `Category` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
